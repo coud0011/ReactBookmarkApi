c7924cad51eb3931afddbaa43763cf7a7cc0607b
coud0011 <axel.coudrot@etudiant.univ-reims.fr>
2024-02-16 13:57:44 +0100

    Ajout de la structure de l'index

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ function App() {
   return (
     <div className="app">
       <header className="">
+        header
       </header>
       <main className="">
+        Main
       </main>
-      <footer className="">footer</footer>
+      <footer className="">
+        footer
+      </footer>
     </div>
   )
 }
