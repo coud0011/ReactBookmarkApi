9138bf58da75830ae4f38aecbe6177f676049867
coud0011 <axel.coudrot@etudiant.univ-reims.fr>
2024-02-16 13:43:04 +0100

    Initialisation du projet

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="app">
+      <header className="">
+      </header>
+      <main className="">
+      </main>
+      <footer className="">footer</footer>
+    </div>
+  )
+}
+
+export default App;
