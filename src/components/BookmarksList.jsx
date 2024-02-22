53c1462c28895fc213e092249dc37a3195578f67
coud0011 <axel.coudrot@etudiant.univ-reims.fr>
2024-02-22 11:03:17 +0100

    Extraction de la pagination

--- src/components/BookmarksList.jsx
+++ src/components/BookmarksList.jsx
@@ -1,15 +1,19 @@
 import React, {useEffect, useState} from "react";
 import {fetchAllBookmarks} from "../services/api/bookmarks.js";
 import BookmarkItem from "./BookmarkItem.jsx";
+import {extractPaginationFromHydraView} from "../services/transformers/paginationFromHydraView.js";
 
 function BookmarksList() {
   const [bookmarksData, setBookmarksData] = useState([]);
+  const [bookmarksPagination, setBookmarksPagination] = useState([]);
   useEffect(() => {
     fetchAllBookmarks().then((collection)=> {
       setBookmarksData(collection['hydra:member']);
+      setBookmarksPagination(collection['hydra:view']);
     })
   },[]);
-  console.log(bookmarksData)
+  let pagination=extractPaginationFromHydraView(bookmarksPagination);
+  console.log(pagination);
   return (
     <section className="bookmarks">
       {bookmarksData.map((bookmark)=> {
---
+++ src/services/transformers/paginationFromHydraView.js
@@ -0,0 +1,32 @@
+import {string} from "prop-types";
+
+export function extractPaginationFromHydraView(hydraView) {
+  if (hydraView['hydra:last'] !== undefined){
+    let searchParams = new URLSearchParams(hydraView['@id'])
+    let hydraId = searchParams.get("/bookmarks/bookmarks?page");
+    searchParams = new URLSearchParams(hydraView['hydra:first'])
+    let hydraFirst = searchParams.get("/bookmarks/bookmarks?page");
+    searchParams = new URLSearchParams(hydraView['hydra:last'])
+    let hydraLast = searchParams.get("/bookmarks/bookmarks?page");
+
+    let hydraPrevious = null;
+    if (hydraView['hydra:previous'] !== undefined) {
+      searchParams = new URLSearchParams(hydraView['hydra:previous'])
+      hydraPrevious = searchParams.get("/bookmarks/bookmarks?page");
+    }
+    let hydraNext = null;
+    if (hydraView['hydra:next'] !== undefined) {
+      searchParams = new URLSearchParams(hydraView['hydra:next'])
+      hydraNext = searchParams.get("/bookmarks/bookmarks?page");
+    }
+    return {
+      current : hydraId,
+      first : hydraFirst,
+      previous : hydraPrevious,
+      next : hydraNext,
+      last : hydraLast
+    };
+  }else {
+    return null;
+  }
+}
